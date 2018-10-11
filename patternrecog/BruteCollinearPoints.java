81a8692e7e3ab5f302189b252f00574c89f19eeb
sonia-yuan <sy010@bucknell.edu>
2018-10-11 18:41:05 -0400

    patternrecog start

---
+++ patternrecog/BruteCollinearPoints.java
@@ -0,0 +1,7 @@
+package patternrecog;
+
+public class BruteCollinearPoints {
+	public BruteCollinearPoints(Point[] points){
+		if (points == null) throw new IllegalArgumentException();
+	}
+}
---
+++ patternrecog/FastCollinearPoints.java
@@ -0,0 +1,5 @@
+package patternrecog;
+
+public class FastCollinearPoints {
+
+}
